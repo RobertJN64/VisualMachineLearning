b53eab996815e5384034c0a65961c75fa0851541
RobertJN <robertjnies@gmail.com>
2021-10-02 19:04:20 -0400

    EmptyNetCreator.py supports tkinter

--- EmptyNetCreator.py
+++ EmptyNetCreator.py
@@ -1,14 +1,61 @@
 import MachineLearning.GeneticNets as gn
+import TKinterModernThemes as TKMT
+import tkinter as tk
+from tkinter import filedialog
 
-def create(file, inputLen, outputLen, midWidth, midDepth, useNeat, useBias, activation_func, fin_activation_func):
-    inputs = []
-    for i in range(0, inputLen):
-        inputs.append("Input" + str(i))
-    outputs = []
-    for i in range(0, outputLen):
-        outputs.append("Output" + str(i))
-    netDB = gn.Random(inputs, outputs, 1, midWidth, midDepth, bias=useBias, neat=useNeat,
-              activation_func=activation_func,
-              final_activation_func=fin_activation_func)
 
-    gn.saveNets([netDB[0][0]], file, "EmptyNet", 1.0)
+class App(TKMT.ThemedTKinterFrame):
+    def __init__(self):
+        super().__init__("Empty Net Creator")
+        frame = self.addLabelFrame("Configuration")
+
+        self.innodesvar = tk.IntVar(value=1)
+        frame.Text("Number of input nodes: ")
+        frame.NumericalSpinbox(1,100,1, self.innodesvar)
+
+        self.outnodesvar = tk.IntVar(value=1)
+        frame.Text("Numer of output nodes: ")
+        frame.NumericalSpinbox(1,100,1,self.outnodesvar)
+
+        self.widthvar = tk.IntVar(value=1)
+        frame.Text("Numer of mid node rows: ")
+        frame.NumericalSpinbox(1,100,1, self.widthvar)
+
+        self.depthvar = tk.IntVar(value=1)
+        frame.Text("Number of mid nodes per row: ")
+        frame.NumericalSpinbox(1,100,1, self.depthvar)
+
+        frame.setActiveCol(1)
+        frame.Text(" ")
+        frame.setActiveCol(2)
+        self.biasvar = tk.BooleanVar()
+        frame.SlideSwitch("Use Bias", self.biasvar)
+
+        self.neatvar = tk.BooleanVar()
+        frame.SlideSwitch("Use Neat", self.neatvar)
+
+        options = ["old", "sigmoid", "relu"]
+        self.actfuncvar = tk.StringVar(value=options[1])
+        self.finactfuncvar = tk.StringVar(value=options[1])
+        frame.Text("Activation Function:")
+        frame.OptionMenu(options, self.actfuncvar, default='sigmoid')
+        frame.Text("Final Activation Function:")
+        frame.OptionMenu(options, self.finactfuncvar, default='sigmoid')
+
+        frame = self.addFrame("Button Frame")
+        frame.AccentButton("Create net!", self.createNet)
+
+    def createNet(self):
+        inputs = []
+        for i in range(0, self.innodesvar.get()):
+            inputs.append("Input" + str(i))
+        outputs = []
+        for i in range(0, self.outnodesvar.get()):
+            outputs.append("Output" + str(i))
+        netDB = gn.Random(inputs, outputs, 1, self.widthvar.get(), self.depthvar.get(), bias=self.biasvar.get(),
+                          neat=self.neatvar.get(), activation_func=self.actfuncvar.get(),
+                          final_activation_func=self.finactfuncvar.get())
+
+        file = filedialog.asksaveasfilename(parent=self.master, title="Save Empty Net",
+                                            filetypes = [["Net Files", "*.json"]])
+        gn.saveNets([netDB[0][0]], file, "EmptyNet", 1.0)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class App(TKMT.ThemedTKinterFrame):
         programs = [["Net Demo", netDemo], ["Genetic Algorithm Demo", genetic], ["Net Analyzer", self.netAnalyzer],
                     ["Interactive Grapher", self.interactiveGrapher], ["Create Report", createReport],
                     ["Create Report Template", createReportTemplate], ["Graph Animation", self.animatedGrapher],
-                    ["Graph 3D", graph3D]]
+                    ["Graph 3D", graph3D], ["Empty Net Creator", emptyNetCreator]]
 
         button_frame = self.addLabelFrame("Programs")
         for name, program in programs:
@@ -28,13 +28,13 @@ class App(TKMT.ThemedTKinterFrame):
         fname = self.openFile("Choose a net file")
         if fname is not None:
             import NetAnalyzer
-            NetAnalyzer.analyze(fname)
+            NetAnalyzer.analyze(fname) #TODO
 
     def interactiveGrapher(self):
         fname = self.openFile("Choose a net file")
         if fname is not None:
             import InteractiveGrapher
-            InteractiveGrapher.graph(fname)
+            InteractiveGrapher.graph(fname) #TODO
 
     def animatedGrapher(self):
         fname = self.openFile("Choose a net file")
@@ -54,67 +54,28 @@ def genetic():
 
 def createReport():
     import ReportGenerator as rg
-    rg.GenerateReport()
+    rg.GenerateReport() #TODO
 
 def createReportTemplate():
     import ReportCreatingAssist as r
-    r.run()
+    r.run() #TODO
 
 def graph3D():
     import Graph3D
-    Graph3D.run()
+    Graph3D.run() #TODO
+
+def emptyNetCreator():
+    import EmptyNetCreator
+    EmptyNetCreator.App()
 #endregion
 
 def old():
     mode = str(input("Net/Genetic/Analyze/Interactive/Report/Template/Graph/Animate/3D: "))
 
-    if mode == "Genetic":
-        pass
-
-
-    elif mode == "Graph":
+    if mode == "Graph":
         import Graph as g
         import json
         config = json.loads(input("Enter graph command: "))
         g.Graph(config)
-
-
-    elif mode == "CreateEmptyNet":
-        #Saves an empty net to a file
-        import EmptyNetCreator
-        file = str(input("Enter ouput file name: "))
-        inputLen = int(input("Number of input nodes: "))
-        outputLen = int(input("Number of output nodes: "))
-        midWidth = int(input("Number of mid node rows: "))
-        midDepth = int(input("Number of mid nodes per row: "))
-
-        useBias = str(input("Use bias? "))
-        if useBias == "" or useBias == "True":
-            useBias = True
-        else:
-            useBias = False
-
-        useNeat = str(input("Use neat? "))
-        if useNeat == "" or useNeat == "False":
-            useNeat = False
-        else:
-            useNeat = True
-
-        activation_func = str(input("Activation function? "))
-        if activation_func == "":
-            activation_func = "old"
-
-        if activation_func not in ["old", "sigmoid", "relu"]:
-            print("ERROR! Activation function not found")
-
-        fin_activation_func = str(input("Final activation function? "))
-        if fin_activation_func == "":
-            fin_activation_func = "old"
-
-        if fin_activation_func not in ["old", "sigmoid", "relu"]:
-            print("ERROR! Activation function not found")
-
-        EmptyNetCreator.create(file, inputLen, outputLen, midWidth, midDepth, useBias, useNeat, activation_func, fin_activation_func)
-
 
 App()
